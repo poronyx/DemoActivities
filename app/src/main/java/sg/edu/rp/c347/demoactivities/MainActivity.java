cbb333b4406c36eee6cd3f67dfc7e4a915e7f9a6
16022550 <Por_-_@hotmail.com>
2018-04-16 21:48:59 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c347/demoactivities/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/demoactivities/MainActivity.java
@@ -1,7 +1,11 @@
 package sg.edu.rp.c347.demoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnDone = (Button) findViewById(R.id.buttonDone);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                EditText etName = (EditText) findViewById(R.id.editTextName);
+                EditText etAge = (EditText) findViewById(R.id.editTextAge);
+
+                String[] info = {etName.getText().toString(), etAge.getText().toString()};
+
+                Intent i = new Intent(MainActivity.this, DemoActivities2.class);
+
+                i.putExtra("info", info);
+
+                startActivity(i);
+            }
+        });
     }
 }
